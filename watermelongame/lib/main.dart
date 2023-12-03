fd5352a14df0c696681665764e31e034df3b0b3c
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2023-12-03 12:48:48 +0900

    add ball on pan end

--- watermelongame/lib/main.dart
+++ watermelongame/lib/main.dart
@@ -1,255 +1,88 @@
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/extensions.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:uuid/uuid.dart';
-import 'package:watermelongame/game.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flutter/widgets.dart';
 
-const supabaseUrl = 'https://nlbsnpoablmsxwkdbmer.supabase.co';
-const supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTE5ODEwMiwiZXhwIjoxOTQ0Nzc0MTAyfQ.XZWLzz95pyU9msumQNsZKNBXfyss-g214iTVAwyQLPA';
-
-void main() async {
-  await Supabase.initialize(
-    url: supabaseUrl,
-    anonKey: supabaseKey,
-    realtimeClientOptions: const RealtimeClientOptions(eventsPerSecond: 30),
+void main() {
+  runApp(
+    const SafeArea(
+        child: GameWidget.controlled(gameFactory: Forge2DExample.new)),
   );
-  runApp(const MyApp());
 }
 
-final supabase = Supabase.instance.client;
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class Forge2DExample extends Forge2DGame with PanDetector {
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'UFO Shooting Game',
-      debugShowCheckedModeBanner: false,
-      home: GamePage(),
-    );
-  }
-}
+  Future<void> onLoad() async {
+    await super.onLoad();
 
-class GamePage extends StatefulWidget {
-  const GamePage({Key? key}) : super(key: key);
-
-  @override
-  State<GamePage> createState() => _GamePageState();
-}
-
-class _GamePageState extends State<GamePage> {
-  late final MyGame _game;
-
-  /// Holds the RealtimeChannel to sync game states
-  RealtimeChannel? _gameChannel;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.asset('assets/images/background.jpg', fit: BoxFit.cover),
-          GameWidget(game: _game),
-        ],
-      ),
-    );
+    camera.viewport.add(FpsTextComponent());
+    world.addAll(createBoundaries());
+    world.add(Ball());
   }
 
   @override
-  void initState() {
-    super.initState();
-    _initialize();
+  void onPanDown(DragDownInfo info) {}
+
+  @override
+  void onPanEnd(DragEndInfo info) {
+    world.add(Ball());
   }
 
-  Future<void> _initialize() async {
-    _game = MyGame(
-      onGameStateUpdate: (position, health) async {
-        ChannelResponse response;
-        do {
-          response = await _gameChannel!.send(
-            type: RealtimeListenTypes.broadcast,
-            event: 'game_state',
-            payload: {'x': position.x, 'y': position.y, 'health': health},
-          );
+  List<Component> createBoundaries() {
+    final visibleRect = camera.visibleWorldRect;
+    final topLeft = visibleRect.topLeft.toVector2();
+    final topRight = visibleRect.topRight.toVector2();
+    final bottomRight = visibleRect.bottomRight.toVector2();
+    final bottomLeft = visibleRect.bottomLeft.toVector2();
 
-          // wait for a frame to avoid infinite rate limiting loops
-          await Future.delayed(Duration.zero);
-          setState(() {});
-        } while (response == ChannelResponse.rateLimited && health <= 0);
-      },
-      onGameOver: (playerWon) async {
-        await showDialog(
-          barrierDismissible: false,
-          context: context,
-          builder: ((context) {
-            return AlertDialog(
-              title: Text(playerWon ? 'You Won!' : 'You lost...'),
-              actions: [
-                TextButton(
-                  onPressed: () async {
-                    Navigator.of(context).pop();
-                    await supabase.removeChannel(_gameChannel!);
-                    _openLobbyDialog();
-                  },
-                  child: const Text('Back to Lobby'),
-                ),
-              ],
-            );
-          }),
+    return [
+      Wall(topLeft, topRight),
+      Wall(topRight, bottomRight),
+      Wall(bottomLeft, bottomRight),
+      Wall(topLeft, bottomLeft),
+    ];
+  }
+}
+
+class Ball extends BodyComponent with TapCallbacks {
+  Ball({Vector2? initialPosition})
+      : super(
+          fixtureDefs: [
+            FixtureDef(
+              CircleShape()..radius = 5,
+              restitution: 0.8,
+              friction: 0.4,
+            ),
+          ],
+          bodyDef: BodyDef(
+            angularDamping: 0.8,
+            position: initialPosition ?? Vector2.zero(),
+            type: BodyType.dynamic,
+          ),
         );
-      },
-    );
 
-    // await for a frame so that the widget mounts
-    await Future.delayed(Duration.zero);
-
-    if (mounted) {
-      _openLobbyDialog();
-    }
-  }
-
-  void _openLobbyDialog() {
-    showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (context) {
-          return _LobbyDialog(
-            onGameStartPressed: (gameId) async {
-              // Add duration to allow subscribing to a new channel in a realtime callback
-              await Future.delayed(Duration.zero);
-
-              setState(() {});
-
-              _game.startNewGame();
-
-              _gameChannel = supabase.channel(gameId,
-                  opts: const RealtimeChannelConfig(ack: true));
-
-              _gameChannel!.on(RealtimeListenTypes.broadcast,
-                  ChannelFilter(event: 'game_state'), (payload, [_]) {
-                final position =
-                    Vector2(payload['x'] as double, payload['y'] as double);
-                final opponentHealth = payload['health'] as int;
-                _game.updateOpponent(
-                  position: position,
-                  health: opponentHealth,
-                );
-
-                if (opponentHealth <= 0) {
-                  if (!_game.isGameOver) {
-                    _game.isGameOver = true;
-                    _game.onGameOver(true);
-                  }
-                }
-              }).subscribe();
-            },
-          );
-        });
+  @override
+  void onTapDown(_) {
+    body.applyLinearImpulse(Vector2.random() * 5000);
   }
 }
 
-class _LobbyDialog extends StatefulWidget {
-  const _LobbyDialog({
-    required this.onGameStartPressed,
-  });
+class Wall extends BodyComponent {
+  final Vector2 _start;
+  final Vector2 _end;
 
-  final void Function(String gameId) onGameStartPressed;
-
-  @override
-  State<_LobbyDialog> createState() => _LobbyDialogState();
-}
-
-class _LobbyDialogState extends State<_LobbyDialog> {
-  late final RealtimeChannel _lobbyChannel;
-  List<String> _userids = [];
-  bool _loading = false;
-
-  /// Unique identifier for each players to identify eachother in lobby
-  final myUserId = const Uuid().v4();
+  Wall(this._start, this._end);
 
   @override
-  void initState() {
-    super.initState();
-
-    _lobbyChannel = supabase.channel(
-      'lobby',
-      opts: const RealtimeChannelConfig(self: true),
+  Body createBody() {
+    final shape = EdgeShape()..set(_start, _end);
+    final fixtureDef = FixtureDef(shape, friction: 0.3);
+    final bodyDef = BodyDef(
+      position: Vector2.zero(),
     );
-    _lobbyChannel.on(RealtimeListenTypes.presence, ChannelFilter(event: 'sync'),
-        (payload, [ref]) {
-      // Update the lobby count
-      final presenceState = _lobbyChannel.presenceState();
 
-      setState(() {
-        _userids = presenceState.values
-            .map((presences) =>
-                (presences.first as Presence).payload['user_id'] as String)
-            .toList();
-      });
-    }).on(RealtimeListenTypes.broadcast, ChannelFilter(event: 'game_start'),
-        (payload, [_]) {
-      // Start the game if someone has started a game with you
-      final participantIds = List<String>.from(payload['participants']);
-      if (participantIds.contains(myUserId)) {
-        final gameId = payload['game_id'] as String;
-        widget.onGameStartPressed(gameId);
-        Navigator.of(context).pop();
-      }
-    }).subscribe(
-      (status, [ref]) async {
-        if (status == 'SUBSCRIBED') {
-          await _lobbyChannel.track({'user_id': myUserId});
-        }
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    supabase.removeChannel(_lobbyChannel);
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Lobby'),
-      content: _loading
-          ? const SizedBox(
-              height: 100,
-              child: Center(child: CircularProgressIndicator()),
-            )
-          : Text('${_userids.length} users waiting'),
-      actions: [
-        TextButton(
-          onPressed: _userids.length < 2
-              ? null
-              : () async {
-                  setState(() {
-                    _loading = true;
-                  });
-
-                  final opponentId =
-                      _userids.firstWhere((userId) => userId != myUserId);
-                  final gameId = const Uuid().v4();
-                  await _lobbyChannel.send(
-                    type: RealtimeListenTypes.broadcast,
-                    event: 'game_start',
-                    payload: {
-                      'participants': [
-                        opponentId,
-                        myUserId,
-                      ],
-                      'game_id': gameId,
-                    },
-                  );
-                },
-          child: const Text('start'),
-        ),
-      ],
-    );
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
 }
